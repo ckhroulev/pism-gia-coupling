6160e00963740c02b12e5ca138568bfc4f714dbf
Constantine Khroulev <ckhroulev@alaska.edu>
2015-05-22 12:40:57 -0800

    CDO seems to recognize grid.nc

--- format_grid.py
+++ format_grid.py
@@ -20,12 +20,36 @@ output.createDimension("lat", N_lat)
 output.createDimension("lon", N_lon)
 output.createDimension("nv", 4)
 nc_lat = output.createVariable("lat", "f8", ("lat",))
+nc_lat.units = "degree_north"
 nc_lon = output.createVariable("lon", "f8", ("lon",))
+nc_lon.units = "degree_east"
 
 nc_latitude  = output.createVariable("latitude", "f8", ("lat", "lon"))
+nc_latitude.units = "degree_north"
 nc_latitude.bounds = "latitude_bnds"
 nc_longitude = output.createVariable("longitude", "f8", ("lat", "lon"))
+nc_longitude.units = "degree_east"
 nc_longitude.bounds = "longitude_bnds"
+
+
+def bogus_data():
+    "Create bogus data to test interpolation."
+    data = np.zeros((N_lat, N_lon))
+
+    W = 8
+    for Lat in xrange(N_lat):
+        if np.mod(Lat, 2*W) < W:
+            data[Lat,:] = 1
+
+    for Lon in xrange(N_lon):
+        if np.mod(Lon, 2*W) < W:
+            data[:,Lon] += 1
+
+    return data
+
+nc_data = output.createVariable("data", "f8", ("lat", "lon"))
+nc_data.coordinates = "latitude longitude"
+nc_data[:] = bogus_data()
 
 nc_lat_bounds = output.createVariable("latitude_bnds", "f8", ("lat", "lon", "nv"))
 nc_lon_bounds = output.createVariable("longitude_bnds", "f8", ("lat", "lon", "nv"))
